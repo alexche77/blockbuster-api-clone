e485260e2eb1f3803dc91713c0a8645148ab18ba
Alvaro <alexche7717@gmail.com>
2021-04-13 09:21:03 -0600

    added special serializer for detaile view if user is staff

--- blockbuster_clone/movies/serializers.py
+++ blockbuster_clone/movies/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from blockbuster_clone.movies.models import Movie
 from rest_framework.validators import UniqueValidator
+
+from blockbuster_clone.movies.models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
@@ -10,7 +11,6 @@ class MovieSerializer(serializers.Serializer):
         validators=[UniqueValidator(queryset=Movie.objects.all())],
     )
     info = serializers.JSONField(read_only=True)
-    sales = serializers.IntegerField(default=0)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
@@ -19,3 +19,12 @@ class MovieSerializer(serializers.Serializer):
         instance.imdb_id = validated_data.get("imdb_id", instance.imdb_id)
         instance.save()
         return instance
+
+
+class MovieDetailSerializer(MovieSerializer):
+    sales = serializers.IntegerField(default=0, read_only=True)
+    rents = serializers.IntegerField(default=0, read_only=True)
+    rent_returns = serializers.IntegerField(default=0, read_only=True)
+    defective_returns = serializers.IntegerField(default=0, read_only=True)
+    purchases = serializers.IntegerField(default=0, read_only=True)
+    adjustments = serializers.IntegerField(default=0, read_only=True)
--- blockbuster_clone/movies/views.py
+++ blockbuster_clone/movies/views.py
@@ -1,22 +1,19 @@
-from rest_framework.views import APIView
-from blockbuster_clone.movies.models import Movie
-from rest_framework.response import Response
-from rest_framework import status
 from django.http import Http404
-from blockbuster_clone.movies.serializers import MovieSerializer
-from blockbuster_clone.movies.permissions import IsStaff
 from rest_framework.mixins import (
+    CreateModelMixin,
     ListModelMixin,
     RetrieveModelMixin,
     UpdateModelMixin,
-    CreateModelMixin,
 )
-
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from structlog import get_logger
 
-from structlog import  get_logger
+from blockbuster_clone.movies.models import Movie
+from blockbuster_clone.movies.serializers import MovieDetailSerializer, MovieSerializer
 
 logger = get_logger()
+
 
 class MovieViewSet(
     RetrieveModelMixin,
@@ -26,20 +23,26 @@ class MovieViewSet(
     GenericViewSet,
 ):
     serializer_class = MovieSerializer
-    queryset = Movie.objects.all()    
+    queryset = Movie.objects.all()
     lookup_field = "imdb_id"
 
     def get_queryset(self):
-        logger.debug('MoviesList', data={'is_staff_member':self.request.user.is_staff_member})
+        logger.debug(
+            "MoviesList", data={"is_staff_member": self.request.user.is_staff_member}
+        )
         if self.request.user.is_staff_member:
             return Movie.objects.all()
         else:
             return Movie.objects.filter(info__Error__isnull=True)
-    
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-        if not self.request.user.is_staff_member and (instance.info is None or 'Error' in instance.info):
+        if not self.request.user.is_staff_member and (
+            instance.info is None or "Error" in instance.info
+        ):
             raise Http404()
-        serializer = self.get_serializer(instance)
+        if self.request.user.is_staff_member:
+            serializer = MovieDetailSerializer(instance)
+        else:
+            serializer = self.get_serializer(instance)
         return Response(serializer.data)
