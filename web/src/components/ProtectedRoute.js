78b42695851d54d1dc7f91cd9e28012edbc139ee
Alvaro <alexche7717@gmail.com>
2021-04-16 09:53:22 -0600

    fe removed

--- web/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-
-const ProtectedRoute = ({isEnabled, ...props}) => {
-    return (isEnabled) ? <Route {...props} /> : <Redirect to="/login"/>;
-};
-
-export default ProtectedRoute;
